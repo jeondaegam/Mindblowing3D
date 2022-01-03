95c395f39c63c47e8f16d4c17d56424ce096fd15
jeondaegam <jeondaegam@naver.com>
2022-01-03 14:42:42 +0900

    3d book

--- README.md
+++ README.md
@@ -5,8 +5,20 @@
 3. Renderer
 
 ## Scene
+Scene => CONTAINER
 
-Scene == CONTAINER
+### Background Color of Scene 
+Scene의 background color를 변경하는 방법   
+1. CSS를 이용한 방법
+```javascript
+// must first set the "alpha" of your WebGLRenderer to "true"
+var renderer = new THREE.WebGLRenderer( {alpha: true} );
+```
+2. renderer
+```javascript
+// 0x000000 색상으로 기본 설정 되어있다. 
+renderer.setClearColor (0xff0000, 1);
+```
 
 ## run dev server
 
---
+++ my-project/aladin.js
@@ -0,0 +1,115 @@
+import * as THREE from 'three';
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+// import {Threex} from "three/examples/jsm/three"
+console.log("hey");
+
+// const frontImg = document.createElement("img");
+// const backImg = document.createElement("img");
+// const spineImg = document.createElement("img");
+
+const canvas = document.querySelector("#app");
+
+// frontImg.src = "./images/front.jpeg";
+// backImg.src = "./images/back.jpeg";
+// spineImg.src = "./images/spine.jpeg";
+//
+// document.body.appendChild(frontImg);
+// document.body.appendChild(spineImg);
+// document.body.appendChild(backImg);
+
+// canvas.innerText = "gogo";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75,
+    window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+// renderer.setClearColor(0xD5EEFF, 1);
+camera.position.setZ(30);
+
+//draw
+renderer.render(scene, camera);
+
+// texture of book
+const frontTexture = new THREE.TextureLoader().load("./images/front.jpeg");
+const spineTexture = new THREE.TextureLoader().load("./images/spine.jpeg");
+const backTexture = new THREE.TextureLoader().load("./images/back.jpeg");
+
+// to create high quality texture
+frontTexture.generateMipmaps = false;
+frontTexture.minFilter = THREE.LinearFilter;
+frontTexture.needsUpdate = true;
+
+const materials = [
+  new THREE.MeshBasicMaterial({map: spineTexture}), // right spine
+  new THREE.MeshBasicMaterial({map: spineTexture}), // left spine
+  new THREE.MeshBasicMaterial({map: frontTexture}),
+  new THREE.MeshBasicMaterial({map: frontTexture}),
+  new THREE.MeshBasicMaterial({map: frontTexture}), // front
+  new THREE.MeshBasicMaterial({map: backTexture}), // back
+];
+
+const book = new THREE.Mesh(
+    new THREE.BoxGeometry(20, 30, 2), materials
+);
+
+scene.add(book);
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(gridHelper);
+
+// mouse orbit controls 효과 mouse로 zoom 가능
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// ==================================
+// const blue = 0x7CC8FD;
+// const green = 0x82FD7C;
+// const red = 0xFD7C7C;
+//
+// const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+// // const material = new THREE.MeshBasicMaterial({wireframe: true}); // the wrapping paper for an object
+// const material = new THREE.MeshStandardMaterial({color: blue}); // need a Hex color : 0x"color name"
+// // meterial에는 색상을 적용해도, embientLight을 넣지 않는 이상 검정색으로 나온다.
+// // combine geometry + material
+// const torus = new THREE.Mesh(geometry, material);
+//
+// scene.add(torus); // 단지 scene에 추가하는 작업일 뿐
+//
+// const ambientLight = new THREE.AmbientLight(0xffffff);
+// scene.add(ambientLight);
+//
+// const pointLight = new THREE.PointLight(green);
+// pointLight.position.set(30, 30, 20);
+// scene.add(pointLight);
+// ==================================\
+let isMouseOver = false;
+book.addEventListener("mouseover", () => {
+  isMouseOver = true;
+});
+book.addEventListener("mouseleave", () => {
+  isMouseOver = false;
+})
+
+function animate() {
+  requestAnimationFrame(animate); // object를 moving
+  // console.log(isMouseOver ? "mouse over" : "mouse leave");
+  renderer.render(scene, camera); // => ** attention! 화면에 render 해준다.
+
+  // rotation position scale
+  // torus.rotation.x += 0.01;
+  // torus.rotation.y += 0.005;
+  // torus.rotation.z += 0.01
+  //
+  // book.rotation.x += 0.02;
+  // book.rotation.y += -0.02;
+  // book.rotation.z += 0.02;
+
+  // constrols.update();
+  // console.log(isClick ? "click" : "not click");
+  // isClick = false;
+}
+
+animate();
