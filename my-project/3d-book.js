a955b0ce50a3645ad275e53c17c5b2ad5b3fec6a
jeondaegam <jeondaegam@naver.com>
2022-01-10 20:49:17 +0900

    3d-book create materials

--- my-project/3d-book.js
+++ my-project/3d-book.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
+import {TextureLoader} from "three";
 //https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=270253933
-// const canvas = document.querySelector("#app");
 
 const section = document.querySelector("section.book");
 // scene
@@ -12,91 +12,91 @@ const camera = new THREE.PerspectiveCamera(75,
 // renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+section.appendChild(renderer.domElement);
 // renderer.setPixelRatio(window.devicePixelRatio);
 // renderer.shadowMap.enabled = true;
 // renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+// renderer.setSize(500,600);
 // renderer.setClearColor(0xffffff,1);
 
 // light
-const ambient = new THREE.AmbientLight(0x404040); // soft white light
+const ambient = new THREE.AmbientLight(0x222222); // soft white light
 scene.add(ambient);
 
 const light = new THREE.DirectionalLight(0xffffff); // direction light
-light.position.set(0, 0, 7);
+light.position.set(0, 0, 6);
+// light.position.set(-10,10,10);
+light.castShadow = true;
 scene.add(light);
-
-//
-const loader = new THREE.TextureLoader();
-
-const urls = [
-  './images/front.jpg',
-  './images/front.jpg',
-  './images/front.jpg',
-  './images/front.jpg',
-  './images/spine.jpg',
-  ' ./images/back.jpg'
-];
-
-const materials = urls.map(url => {
-  return new THREE.MeshLambertMaterial({
-    map: loader.load(url)
-  })
-});
-
-// geometry
-const geometry = new THREE.BoxGeometry(3.5, 5, 0.5);
-const cube = new THREE.Mesh(geometry, materials);
-scene.add(cube);
 
 camera.position.z = 6;
 
-// img url list, getBookMaterials를 호출할 때 사용할 element
-// const bookCovers = {
-//   front: "./images/front.jpg",
-//   spine: "./images/spine.jpg",
-//   back: "./images/back.jpg"
-// };
+const urls = {
+  front: './images/front.jpg',
+  spine: './images/spine.jpg',
+  back: ' ./images/back.jpg'
+};
 
-// function getBookMaterials(urlMap) {
-//   const materialMap = ['edge', 'spine', 'top', 'bottom', 'front', 'back'];
-//
-//   // name list 를 map 으로 불러온다? map 안의 element 이름은 name 이다.
-//   return materialMap.map(
-//       (name) => {
-//         if (!urlMap[name]) {
-//           return new THREE.MeshBasicMaterial({color: 0xffffff});
-//         }
-//
-//         const texture = new THREE.TextureLoader().load(urlMap[name]);
-//
-//         // to create high quality texture
-//         texture.generateMipmaps = false;
-//         texture.miniFilter = THREE.LinearFilter;
-//         texture.needsUpdate = true;
-//
-//         return new THREE.MeshBasicMaterial({map: texture});
-//
-//       }
-//   );
-// }
+// geometry
+const geometry = new THREE.BoxGeometry(2.5, 4, 0.5);
 
-// const book = new THREE.Mesh(
-//     new THREE.BoxGeometry(15, 20, 2), getBookMaterials(bookCovers));
-// book.castShadow = true;
-// scene.add(book);
+function getBookMaterials(urls) {
+  const materialNames = ['edge', 'spine', 'top', 'bottom', 'front', 'back']; // 배열
+
+  // get a material one by one
+  return materialNames.map((name) => {
+    if(!urls[name]) {
+      return new THREE.MeshBasicMaterial({color:0xE1D9D9});
+    }
+
+    // create a texture
+    const texture = new TextureLoader().load(urls[name]);
+    texture.generateMipmaps = false;
+    texture.miniFilter = THREE.LinearFilter;
+    texture.needsUpdate = true;
+    return new THREE.MeshLambertMaterial({map: texture});
+  });
+}
+
+const cube = new THREE.Mesh(geometry, getBookMaterials(urls));
+scene.add(cube);
+
+
+//===
+// scene.background = new THREE.Color(0xffffff);
+//
+// const planeGeometry = new THREE.PlaneGeometry(500, 500, 32, 32);
+// const planeMaterial = new THREE.ShadowMaterial();
+// planeMaterial.opacity = 0.5;
+// const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+// plane.receiveShadow = true;
+// scene.add(plane);
+//
+// const light = new THREE.DirectionalLight(0xffffff); // direction light
+// // light.position.set(0, 0, 6);
+// light.position.set(-10,10,10);
+// light.castShadow = true;
+// scene.add(light);
+//
+// const geometry = new THREE.BoxGeometry(2.5, 4, 0.5);
+// const cube = new THREE.Mesh(geometry, getBookMaterials(urls));
+// cube.castShadow = true;
+// cube.position.set(0,0,0);
+//
+// scene.add(cube);
+//===
 
 // Smoothing the animation on scroll
-let currentTimeline = window.pageYOffset / 3000;
+// let currentTimeline = window.pageYOffset / 3000;
 let aimTimeline = window.pageYOffset / 3000;
 
 // Rendering the scene
 function animate() {
   requestAnimationFrame(animate); // move object
 
-  currentTimeline += (aimTimeline - currentTimeline) * 0.01;
+  // currentTimeline += (aimTimeline - currentTimeline) * 0.01;
 
-  // const currentTimeline = window.pageYOffset / 3000;
+  const currentTimeline = window.pageYOffset / 3000;
   // const rx = currentTimeline * Math.PI *2;
   const rx = currentTimeline * -0.5 + 0.5;
   // const ry = currentTimeline * Math.PI *2;
@@ -105,8 +105,8 @@ function animate() {
   cube.rotation.set(rx, ry, 0);
 
   // Animating the cube
-  // cube.rotation.x += 0.01;
-  // cube.rotation.y += 0.01;
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
   renderer.render(scene, camera); // => 실제 화면에 rendering 해주는 역할
 
 };
