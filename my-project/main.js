9241e5ddd81e9c7fea602971910e13c8ab934d42
jeondaegam <jeondaegam@naver.com>
2022-01-12 16:52:22 +0900

    PointsMaterial 사용해보기

--- my-project/main.js
+++ my-project/main.js
@@ -22,20 +22,23 @@ renderer.render(scene, camera);
 // 원하는 모양의 geometry
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 // const material = new THREE.MeshBasicMaterial({wireframe: true}); // the wrapping paper for an object
-const material = new THREE.MeshStandardMaterial({color: 0x6FA8DC}); // need a Hex color : 0x"color name"
+const material = new THREE.PointsMaterial({
+  color: 0xffffff,
+  size: 0.01
+}); // need a Hex color : 0x"color name"
 // combine geometry + material
-const torus = new THREE.Mesh(geometry, material);
+const torus = new THREE.Points(geometry, material);
 
 scene.add(torus);
 
 // 조명
-const pointLight = new THREE.PointLight(0xffffff);
+const pointLight = new THREE.PointLight(0xffffff, 0.1);
 pointLight.position.set(30, 30, 20);
 
 // flat light in the room , object 전체에 은은한 조명효과!
 // everything seem equally.
 const ambientLight = new THREE.AmbientLight(0xD8A2F7);
-// scene.add(pointLight);
+// // scene.add(pointLight);
 scene.add(ambientLight, pointLight);
 
 //
@@ -132,7 +135,7 @@ const grassBlock = new THREE.Mesh(
     materials);
 
 // grassBlock.position.set(-20, 20);
-scene.add(grassBlock);
+// scene.add(grassBlock);
 // new block ======================================================
 
 // animation loop (동작이 loop 될 수 있게)
